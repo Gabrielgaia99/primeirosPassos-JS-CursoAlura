a70ed25db62f8daa5a8504118ee8b1dfd62cead3
Gabrielgaia99 <Gabrielgaiapereira@gmail.com>
2022-02-07 08:15:13 -0300

    Loops
    Finalização do Curso

--- 8-loops.js
+++ 8-loops.js
@@ -57,6 +57,23 @@ while(contador<3){
 }
     console.log("Destino Existe: ", destinoExiste);
 
+    if(podeComprar && destinoExiste){
+        console.log("Boa viagem");
+    }else{
+        console.log("Desculpe tivemos um erro!")
+    }
+
+    for(let cont = 0 ;cont < 3; cont++ ){
+    
+        if(listaDeDestinos[contador] == destino){
+            console.log("Destino existe");
+            destinoExiste = true;
+            
+        }
+        
+    
+    }
+
 
 
 console.log(listaDeDestinos);
